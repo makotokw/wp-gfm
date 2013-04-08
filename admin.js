5887b662d7f153facf8530fdf61d86b5c32b1b78
makoto_kw <makoto.kw@gmail.com>
2013-04-08 21:09:18 +0900

    Fixed edit button

--- admin.js
+++ admin.js
@@ -1,23 +1,18 @@
-(function($) {
-    if (typeof $ == "undefined") return;
-    $(document).ready(function(){
-        var i = edButtons.length;
-        edButtons[i] = new edButton(
-                'ed_markdown',
-                'p',
-                '[markdown]\n',
-                '[/markdown]\n',
-                'p');
-        var $button = $('<input/>')
-            .attr({type:'button',
-                id: edButtons[i].id,
-                className: 'ed_button',
-                value: 'markdown',
-                title: 'Markdown'
-            }).click(function(e) {
-                edInsertTag(edCanvas, i);
-            })
-            ;
-        $('#ed_code').after($button);
-    });
+(function ($) {
+	if (typeof $ == "undefined") return;
+	$(document).ready(function () {
+		var length = edButtons.length;
+		var $toolbar = $('#ed_toolbar');
+		$.each(['markdown', 'gfm'], function(index, sh){
+			var id = 'ed_' + sh;
+			var eb = new edButton(
+				id,
+				sh,
+				'[' + sh + ']\n',
+				'[/' + sh + ']\n',
+				-1
+			);
+			edButtons.push(eb);
+		});
+	});
 })(jQuery)
